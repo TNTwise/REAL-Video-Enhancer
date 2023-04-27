8cb402919663914b838da5b689fa32fca6a2ea21
Paxson Gottschalk <tntwise3@gmail.com>
2023-04-26 21:31:10 -0500

    deleted:    GUI.py 	deleted:    distro.py 	deleted:    install_dependencies.py 	deleted:    installer.py 	deleted:    main.py 	deleted:    test.py

--- main.py
+++
@@ -1,8 +0,0 @@
- 
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtGui as qtg
-import os 
-class MainWindow(qtw.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setLayout(qtw.QVBoxLayout())
