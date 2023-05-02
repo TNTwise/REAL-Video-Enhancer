17881f60c7405b22d42cd4f8cba13bcef81d243b
Paxson Gottschalk <tntwise3@gmail.com>
2023-05-01 20:14:52 -0500

    modified:   main.py 	modified:   mainwindow.ui

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from PyQt5 import QtWidgets, uic
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+import mainwindow
 
 
-class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('mainwindow.ui', self) # Load the .ui file
-        self.show() # Show the GUI
-
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
-window = Ui() # Create an instance of our class
-app.exec_() # Start the application
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = mainwindow.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
