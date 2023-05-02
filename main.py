4b81c35baba3e86736535206df011af1c54e8eb6
TNTwise <77559100+TNTwise@users.noreply.github.com>
2023-05-02 02:34:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 import mainwindow
 import os
+import start
 thisdir = os.getcwd()
 homedir = os.path.expanduser(r"~")
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        
-        
-        
-        ui = mainwindow.Ui_MainWindow()
-        ui.setupUi(self)
-
-        ui.Input_video_rife.clicked.connect(self.openFileNameDialog)
+        self.ui = mainwindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.pin_functions()
         self.show()
+
+    def pin_functions(self):
+
+        self.ui.Input_video_rife.clicked.connect(self.openFileNameDialog)
+        self.ui.Output_folder_rife.clicked.connect(self.openFolderDialog)
+        self.ui.Rife_Model.setCurrentIndex(5)
+
     def openFileNameDialog(self):
+
+        self.input_file = QFileDialog.getOpenFileName(self, 'Open File', f'{homedir}',"Video files (*.mp4);;All files (*.*)")
+    
+    def openFolderDialog(self):
         
-        fname = QFileDialog.getOpenFileName(self, 'Open file', 
-    f'{homedir}',"Video files (*.mp4)")
+        self.output_folder = QFileDialog.getExistingDirectory(self, 'Open Folder')
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
