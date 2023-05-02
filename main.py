c541730f1cc41cd3be4a58622f3034973576a8d3
TNTwise <77559100+TNTwise@users.noreply.github.com>
2023-05-02 01:50:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ from PyQt5 import QtWidgets, uic
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 import mainwindow
+import os
+thisdir = os.getcwd()
+homedir = os.path.expanduser(r"~")
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        
+        
+        
+        ui = mainwindow.Ui_MainWindow()
+        ui.setupUi(self)
 
-if __name__ == "__main__":
-    import sys
+        ui.Input_video_rife.clicked.connect(self.openFileNameDialog)
+        self.show()
+    def openFileNameDialog(self):
+        
+        fname = QFileDialog.getOpenFileName(self, 'Open file', 
+    f'{homedir}',"Video files (*.mp4)")
+
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = mainwindow.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    window = MainWindow()
     sys.exit(app.exec_())
