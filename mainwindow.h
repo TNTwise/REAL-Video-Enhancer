bfe270686dd31f186fcb6098a2b09429a7e56dda
Paxson Gottschalk <tntwise3@gmail.com>
2023-05-01 19:49:42 -0500

    modified:   mainwindow.h 	modified:   mainwindow.ui

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
