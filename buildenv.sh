f0108f658f5c66983c3a8c69635450929535aac0
TNTwise <77559100+TNTwise@users.noreply.github.com>
2023-05-29 15:48:39 -0500

    Update buildenv.sh

--- buildenv.sh
+++ buildenv.sh
@@ -1,2 +1,2 @@
-python -m PyQt5.uic.pyuic mainwindow.ui > mainwindow.py
-python -m PyQt5.uic.pyuic src/SelectModels.ui > src/SelectModels.py
+python3 -m PyQt5.uic.pyuic mainwindow.ui > mainwindow.py
+python3 -m PyQt5.uic.pyuic src/SelectModels.ui > src/SelectModels.py
