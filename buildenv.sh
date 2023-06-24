140067652ec2448d5fc88293f0d9502f22369a6f
Paxson Gottschalk <tntwise3@gmail.com>
2023-06-23 22:04:16 -0500

    adds requirements.txt for pip

--- buildenv.sh
+++ buildenv.sh
@@ -1,6 +1,4 @@
-pip install opencv-python-headless
-#pip install opencv-python
-pip install pyqt5
+
 
 python3 -m PyQt5.uic.pyuic mainwindow.ui > mainwindow.py
 python3 -m PyQt5.uic.pyuic src/SelectModels.ui > src/SelectModels.py
