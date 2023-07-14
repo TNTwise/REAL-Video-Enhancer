82cef265211915cd2fae6d9ec2e54a59b3b9cff0
TNTwise <tntwise3@gmail.com>
2023-07-14 07:03:44 -0500

    make new installer better ig

--- buildenv.sh
+++ buildenv.sh
@@ -2,3 +2,4 @@
 
 python3 -m PyQt5.uic.pyuic mainwindow.ui > mainwindow.py
 python3 -m PyQt5.uic.pyuic src/getModels/SelectModels.ui > src/getModels/SelectModels.py
+python3 -m PyQt5.uic.pyuic src/getModels/Download.ui > src/getModels/Download.py
--- src/getModels/select_models.py
+++ src/getModels/select_models.py
@@ -13,8 +13,44 @@ from PyQt5.QtGui import QIcon
 import src.messages
 from threading import Thread
 import src.getModels.SelectModels as SelectModels
+import src.getModels.Download as DownloadUI
 global rife_install_list
+from PyQt5.QtCore import QThread, pyqtSignal, QObject, pyqtSlot
+
 rife_install_list=[]
+class Worker(QObject):
+    finished = pyqtSignal()
+    intReady = pyqtSignal(list)
+    
+                    
+    @pyqtSlot()
+    def install_modules(self):
+                    install_modules_dict={
+                                        
+'https://github.com/nihui/realcugan-ncnn-vulkan/releases/download/20220728/realcugan-ncnn-vulkan-20220728-ubuntu.zip':'realcugan-ncnn-vulkan-20220728-ubuntu.zip',
+'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-ubuntu.zip':'realesrgan-ncnn-vulkan-20220424-ubuntu.zip',
+'https://github.com/nihui/cain-ncnn-vulkan/releases/download/20220728/cain-ncnn-vulkan-20220728-ubuntu.zip':'cain-ncnn-vulkan-20220728-ubuntu.zip',
+'https://raw.githubusercontent.com/TNTwise/Rife-Vulkan-Models/main/rife-ncnn-vulkan':'rife-ncnn-vulkan'}
+                    for i in rife_install_list:
+                                        install_modules_dict[f'https://raw.githubusercontent.com/TNTwise/Rife-Vulkan-Models/main/{i}.tar.gz'] = f'{i}.tar.gz'
+                    total_size_in_bytes=0
+                    data_downloaded=0
+                    for link,name in install_modules_dict.items():
+                        response = requests.get(link, stream=True)
+                        total_size_in_bytes+= int(response.headers.get('content-length', 0))
+                    
+                    for link,name in install_modules_dict.items():
+                        response = requests.get(link, stream=True)
+                        with open(f'{thisdir}/files/{name}', 'wb') as f:
+                                for data in response.iter_content(1024):
+                                    f.write(data)
+                                    data_downloaded+=1024
+                                    self.intReady.emit([int(data_downloaded),total_size_in_bytes]) # sends back data to main thread
+                    
+                    
+                    
+                    self.finished.emit()
+
 if os.path.isfile(f'{thisdir}/realesrgan-vulkan-models/realesrgan-ncnn-vulkan') == False or os.path.isfile(f'{thisdir}/rife-vulkan-models/rife-ncnn-vulkan') == False:
     
     class ChooseModels(QtWidgets.QMainWindow):
@@ -81,43 +117,60 @@ if os.path.isfile(f'{thisdir}/realesrgan-vulkan-models/realesrgan-ncnn-vulkan') 
     window = ChooseModels()
     app.exec_()
     if len(rife_install_list) > 0:
-        import os
-        thisdir=os.getcwd()
-        import requests
-        
-        def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-            
-            percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-            filledLength = int(length * iteration // total)
-            bar = fill * filledLength + '-' * (length - filledLength)
-            print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-            # Print New Line on Complete
-            i=0
-            if iteration >= total and i == 0: 
-                print('\nDownloaded!')
-                i=1
-        def install_modules(links_and_names_dict):
-            total_size_in_bytes=0
-            data_downloaded=0
-            for link,name in links_and_names_dict.items():
-                response = requests.get(link, stream=True)
-                total_size_in_bytes+= int(response.headers.get('content-length', 0))
-            for link,name in links_and_names_dict.items():
-                response = requests.get(link, stream=True)
-                with open(f'{thisdir}/files/{name}', 'wb') as f:
-                        for data in response.iter_content(1024):
-                            f.write(data)
-                            data_downloaded+=1024
-                            printProgressBar(data_downloaded/total_size_in_bytes*100,100)
-        install_modules_dict={
+        class Downloading(QtWidgets.QMainWindow):
+                def __init__(self):
+                    super(Downloading, self).__init__()
+                    self.ui = DownloadUI.Ui_MainWindow()
+                    self.ui.setupUi(self)
+                    self.show()
+                    self.nextfunction()
+                    
+                def showDialogBox(self,message,displayInfoIcon=False):
+                    icon = QIcon(f"{thisdir}/icons/Rife-ESRGAN-Video-Settings - Info.png")
+                    msg = QMessageBox()
+                    msg.setWindowTitle(" ")
+                    if displayInfoIcon == True:
+                        msg.setIconPixmap(icon.pixmap(32, 32)) 
+                    msg.setText(f"{message}")
+                    
+                    msg.exec_()
                 
-                'https://github.com/nihui/realcugan-ncnn-vulkan/releases/download/20220728/realcugan-ncnn-vulkan-20220728-ubuntu.zip':'realcugan-ncnn-vulkan-20220728-ubuntu.zip',
-                'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-ubuntu.zip':'realesrgan-ncnn-vulkan-20220424-ubuntu.zip',
-                'https://github.com/nihui/cain-ncnn-vulkan/releases/download/20220728/cain-ncnn-vulkan-20220728-ubuntu.zip':'cain-ncnn-vulkan-20220728-ubuntu.zip',
-                'https://raw.githubusercontent.com/TNTwise/Rife-Vulkan-Models/main/rife-ncnn-vulkan':'rife-ncnn-vulkan'}
-        for i in rife_install_list:
-            install_modules_dict[f'https://raw.githubusercontent.com/TNTwise/Rife-Vulkan-Models/main/{i}.tar.gz'] = f'{i}.tar.gz'
-            
-        install_modules(install_modules_dict)
+                def nextfunction(self):
+                    
+                    logo = QIcon(f"{thisdir}/icons/logo v1.png")
+                    
+                    self.ui.logoPreview.setPixmap(logo.pixmap(256,256))
+                    
+                    self.obj = Worker()
+                    self.thread = QThread()
+                    self.obj.intReady.connect(self.on_count_changed)
+                    self.obj.moveToThread(self.thread)
+                    self.obj.finished.connect(self.thread.quit)
+                    self.thread.started.connect(self.obj.install_modules)
+                    self.thread.start()
+                    self.obj.finished.connect(self.start_main)
+                def on_count_changed(self,list):
+                    downloaded_data = list[0]
+                    total_data = list[1]
+                    self.ui.downloadProgressBar.setMaximum(total_data)
+                    self.ui.downloadProgressBar.setValue(downloaded_data)
+                    downloaded_data_gb = str(downloaded_data/1000000000)[:4]
+                    total_data_gb = str(total_data/1000000000)[:4]
+                    
+                    self.ui.gbLabel.setText(f'{downloaded_data_gb}/{total_data_gb}GB')
+                def start_main(self):
+                    QApplication.closeAllWindows()
+                    return 0
+
+        import src.theme as theme
+        
+        app1 = QtWidgets.QApplication(sys.argv)
+        theme.set_theme(app1)
+        
+        
+        window = Downloading()
+        app1.exec_()
+        
+        
     else:
         exit()
