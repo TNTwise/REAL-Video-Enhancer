2c0547c6083c9000aba5384951390739fb21a76e
TNTwise <tntwise@github.com>
2023-08-10 18:52:29 -0500

    add in better implementation of storage warning

---
+++ modules/interpolate.py
@@ -0,0 +1,104 @@
+#This script creates a class that takes in params like "RealESRGAN or Rife", the model for the program,  the times of upscaling, and the path of the video, and the output path
+# hz
+import src.return_data as return_data
+import os
+from src.settings import *
+import glob
+from threading import Thread
+import src.runAI.transition_detection
+from src.return_data import *
+from src.messages import *
+from src.discord_rpc import *
+import glob
+import os
+from modules.commands import *
+import src.thisdir
+thisdir = src.thisdir.thisdir()
+homedir = os.path.expanduser(r"~")
+
+
+def run_start(self,AI):
+    os.system(f'rm -rf "{self.render_folder}/{self.videoName}_temp/"')
+    self.ui.QueueButton.show()
+    
+    
+
+    self.AI = AI
+    settings = Settings()
+    self.setDisableEnable(True)
+    
+    if settings.DiscordRPC == 'Enabled':
+        start_discordRPC(self,'Interpolating')
+    
+    self.ui.logsPreview.append(f'Extracting Frames')
+    
+    
+    self.rifeThread = Thread(target=lambda: start_Render(self,(self.ui.Rife_Model.currentText().lower()),self.times,self.input_file,self.output_folder))
+    
+    self.rifeThread.start()
+    self.runPB()
+
+def start_interpolation(self,AI): #should prob make this different, too similar to start_rife but i will  think of something later prob
+    
+    # Calculate the aspect ratio
+                
+        
+        if self.input_file != '':
+            self.render='rife'
+            has_enough_space,predicted_space,total_space = checks.check_if_enough_space(self.input_file,self.render,self.times)
+            
+            if has_enough_space:
+                run_start(self,AI)
+            elif not_enough_storage(self,predicted_space,total_space):
+                run_start(self,AI)
+            else:
+                pass
+                 
+        else:
+             no_input_file(self)
+             
+            
+
+    
+def start_Render(self,model,times,videopath,outputpath):
+        
+        
+        
+        self.ui.ETAPreview.setText('ETA:')
+        self.ui.processedPreview.setText('Files Processed:')
+        
+        # Have to put this before otherwise it will error out ???? idk im not good at using qt.....
+                
+                
+        #self.runLogs(videoName,times)
+        start(self,self.render_folder,self.videoName,videopath,times)
+        self.transitionDetection = src.runAI.transition_detection.TransitionDetection(self)
+        self.transitionDetection.find_timestamps()
+        self.transitionDetection.get_frame_num(times)
+        self.endNum = 0 # This variable keeps track of the amound of zeros to fill in the output frames, this helps with pausing and resuming so rife wont overwrite the original frames.
+        Render(self,model,times,videopath,outputpath)
+        
+        
+        
+        
+        
+    
+        
+def Render(self,model,times,videopath,outputpath):   
+        self.paused = False
+        settings=Settings()
+        #Thread(target=self.calculateETA).start()
+        input_frames = len(os.listdir(f'{self.render_folder}/{self.videoName}_temp/input_frames/'))
+        if self.AI == 'rife-ncnn-vulkan':
+            if model == 'rife-v4.6' or model == 'rife-v4':
+                os.system(f'"{settings.ModelDir}/rife/rife-ncnn-vulkan" -n {input_frames*times}  -m  {model} -i "{self.render_folder}/{self.videoName}_temp/input_frames/" -o "{self.render_folder}/{self.videoName}_temp/output_frames/" {return_gpu_settings(self)} -f %08d{self.settings.Image_Type}')
+            else:
+                os.system(f'"{settings.ModelDir}/rife/rife-ncnn-vulkan"  -m  {model} -i "{self.render_folder}/{self.videoName}_temp/input_frames/" -o "{self.render_folder}/{self.videoName}_temp/output_frames/" {return_gpu_settings(self)} -f %08d{self.settings.Image_Type} ')
+        if os.path.exists(f'{self.render_folder}/{self.videoName}_temp/output_frames/') == False:
+             show_on_no_output_files(self)
+        
+        else:
+            self.transitionDetection.merge_frames()
+            
+            self.output_file = end(self,self.render_folder,self.videoName,videopath,times,outputpath, self.videoQuality,self.encoder)
+ 
---
+++ modules/upscale.py
@@ -0,0 +1,60 @@
+
+import src.return_data as return_data
+import os
+from src.settings import *
+import glob
+from threading import Thread
+import src.runAI.transition_detection
+from src.return_data import *
+from src.messages import *
+from src.discord_rpc import *
+import glob
+import os
+from modules.commands import *
+from cv2 import VideoCapture, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS, CAP_PROP_FRAME_COUNT
+
+import src.thisdir
+thisdir = src.thisdir.thisdir()
+homedir = os.path.expanduser(r"~")
+def startRender(self):
+
+    start(self,self.render_folder,self.videoName,self.input_file,1)
+    
+    realESRGAN(self)
+def realESRGAN(self):
+        settings = Settings()
+        self.endNum=0
+        self.paused=False
+        img_type = self.settings.Image_Type.replace('.','')
+        if self.AI == 'realesrgan-ncnn-vulkan':
+            os.system(f'"{settings.ModelDir}/realesrgan/realesrgan-ncnn-vulkan" -i "{self.render_folder}/{self.videoName}_temp/input_frames" -o "{self.render_folder}/{self.videoName}_temp/output_frames" {self.realESRGAN_Model}{return_gpu_settings(self)} -f {img_type} ')
+        if os.path.exists(f'{self.render_folder}/{self.videoName}_temp/output_frames/') == False:
+                show_on_no_output_files(self)
+        else:
+                if self.paused == False:
+                    self.output_file = end(self,self.render_folder,self.videoName,self.input_file,1,self.output_folder, self.videoQuality,self.encoder,'upscale')
+                else:
+                    pass
+    
+def start_upscale(self,AI):
+    if self.input_file != '':
+        
+        self.ui.QueueButton.show()
+        self.render='esrgan'
+        self.AI = AI
+        settings = Settings()
+        self.setDisableEnable(True)
+        
+        if settings.DiscordRPC == 'Enabled':
+            start_discordRPC(self,'Upscaling')
+        self.ui.logsPreview.append(f'Extracting Frames')
+            
+        realESRGAN_Model = self.ui.Rife_Model.currentText()
+        realESRGAN_Times = self.ui.Rife_Times.currentText()
+        if AI == 'realesrgan-ncnn-vulkan':
+            if realESRGAN_Model == 'Default':
+                self.realESRGAN_Model = '-n realesrgan-x4plus -s 4'
+            if realESRGAN_Model == 'Animation':
+                self.realESRGAN_Model = f'-n realesr-animevideov3 -s {realESRGAN_Times}'
+        Thread(target=lambda: startRender(self)).start()
+        self.runPB()
