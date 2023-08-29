32c1610e3b7c8220701fb70e5380a650ccd4857a
TNTwise <tntwise@github.com>
2023-08-29 12:37:26 -0500

    add files

---
+++ src/log.py
@@ -0,0 +1,22 @@
+import src.thisdir
+import datetime
+import os
+current_time = datetime.datetime.now().time()
+
+thisdir = src.thisdir.thisdir()
+try:
+    os.mkdir(f'{thisdir}/logs/')
+except:
+    if len(os.listdir(f'{thisdir}/logs/')) > 4:
+        oldest_file = min(os.listdir(f'{thisdir}/logs/'), key=lambda x: os.path.getctime(os.path.join(f'{thisdir}/logs/', x)))
+        os.remove(f'{thisdir}/logs/{oldest_file}')
+try:
+    os.system(f'touch "{thisdir}/logs/log{current_time}.txt"')
+except:
+    pass
+def log(log):
+    
+    with open(f'{thisdir}/logs/log_{current_time}.txt', 'a') as f:
+        f.write(log + '\n')
+
+    
