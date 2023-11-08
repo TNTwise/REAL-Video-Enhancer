bad255846f93b0c4c32e377efa0f58e0a5293071
Paxson Gottschalk <tntwise3@gmail.com>
2023-11-08 15:47:45 -0600

    just limit jpg errors at this point

--- src/workers.py
+++ src/workers.py
@@ -537,9 +537,8 @@ class upscale(QObject):
             if self.main.settings.FrameIncrementsMode == 'Manual':
                 frame_increments_of_interpolation = self.main.settings.FrameIncrements
             elif self.main.settings.FrameIncrementsMode == 'Automatic':
-                resolution = VideoName.return_video_resolution(self.main.input_file)
                 
-                frame_increments_of_interpolation = int(10*int(self.main.settings.VRAM))
+                frame_increments_of_interpolation = int(10*int(self.main.settings.VRAM/2))
                 
                 frame_increments_of_interpolation = int(frame_increments_of_interpolation)
             self.main.frame_increments_of_interpolation = frame_increments_of_interpolation
