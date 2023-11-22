b4baeedaa31aafdc4537e1bc4236f7030da35035
Paxson Gottschalk <tntwise3@gmail.com>
2023-11-22 15:42:24 -0600

    change install directory permidently

--- src/thisdir.py
+++ src/thisdir.py
@@ -1,5 +1,6 @@
 import os
 homedir =  os.path.expanduser(r"~")
-
+if os.path.exists(f'{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer') == False:
+    os.system(f'mkdir -p "{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"')
 def thisdir():
     return (f'{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer')
