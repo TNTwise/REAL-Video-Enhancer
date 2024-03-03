02b5467aa7ed0c4785e41d4fa2e28b3c0d611e70
Paxson Gottschalk <tntwise3@gmail.com>
2024-03-03 15:24:10 -0600

    more logs

--- main.py
+++ main.py
@@ -65,14 +65,13 @@ import modules.interpolate as interpolate
 import modules.upscale as upscale
 
 from src.misc.log import log
-try:
     import torch
     import torchvision
     torch_version = True
     log('torch_version')
 
-except:
-    log('ncnn_verson')
+except Exception as e:
+    log(f'ncnn_verson {e}')
     torch_version = False
 if torch_version:
     import modules.RifeCUDA as rifeCUDA
