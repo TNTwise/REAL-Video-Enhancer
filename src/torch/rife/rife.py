fef2d16779fee968631dfbc0325eca02bc1dcee4
TNTwise <tntwise3@gmail.com>
2024-03-09 18:30:55 -0600

    fix rife preview

--- src/torch/rife/rife.py
+++ src/torch/rife/rife.py
@@ -94,7 +94,6 @@ class Rife:
             .float()
             / 255.0
         )
-        print(self.I0.shape)
         self.I1 = (
             torch.from_numpy(I1)
             .to(self.device, non_blocking=True)
