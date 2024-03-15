20d2194555f2e85ba0cfc9b06b43bad53b134177
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-03-15 02:54:45 +0000

    Update thisdir.py

--- src/programData/thisdir.py
+++ src/programData/thisdir.py
@@ -1,13 +1,13 @@
 import os
 import platform
 homedir = os.path.expanduser(r"~")
-os = platform.system()
+operating_system = platform.system()
 global flatpak
-if os == 'Darwin':
+if operating_system == 'Darwin':
     flatpak = False
     if os.path.exists(f"{homedir}/Library/REAL-Video-Enhancer") == False:
         os.system(f'mkdir -p "{homedir}/Library/REAL-Video-Enhancer"')
-if os == 'Linux':
+if operating_system == 'Linux':
     if os.path.exists(f"{homedir}/.local/share/REAL-Video-Enhancer") == False:
         os.system(f'mkdir -p "{homedir}/.local/share/REAL-Video-Enhancer"')
     
@@ -17,11 +17,11 @@ if os == 'Linux':
 
 
 def thisdir():
-    if os == 'Linux':
+    if operating_system == 'Linux':
         if flatpak == True:
             if os.path.exists(f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer") == False:
                 os.system(f'mkdir -p "{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"')
             return f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"
         return f"{homedir}/.local/share/REAL-Video-Enhancer"
-    if os == 'Darwin':
+    if operating_system == 'Darwin':
         return f"{homedir}/Library/REAL-Video-Enhancer"
