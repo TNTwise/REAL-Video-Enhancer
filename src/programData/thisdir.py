0241c451a04152e04666d7d3c984d360557c4949
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-03-15 02:53:34 +0000

    Update thisdir.py

--- src/programData/thisdir.py
+++ src/programData/thisdir.py
@@ -1,17 +1,26 @@
 import os
-
+import platform
 homedir = os.path.expanduser(r"~")
-if os.path.exists(f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer") == False:
-    os.system(f'mkdir -p "{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"')
-if os.path.exists(f"{homedir}/.local/share/REAL-Video-Enhancer") == False:
-    os.system(f'mkdir -p "{homedir}/.local/share/REAL-Video-Enhancer"')
-global flatpak
-flatpak = False
-if "FLATPAK_ID" in os.environ:
-    flatpak = True
+os = platform.system()
+if os == 'Darwin':
+    flatpak = False
+    if os.path.exists(f"{homedir}/Library/REAL-Video-Enhancer") == False:
+        os.system(f'mkdir -p "{homedir}/Library/REAL-Video-Enhancer"')
+if os == 'Linux':
+    if os.path.exists(f"{homedir}/.local/share/REAL-Video-Enhancer") == False:
+        os.system(f'mkdir -p "{homedir}/.local/share/REAL-Video-Enhancer"')
+    global flatpak
+    flatpak = False
+    if "FLATPAK_ID" in os.environ:
+        flatpak = True
 
 
 def thisdir():
-    if flatpak == True:
-        return f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"
-    return f"{homedir}/.local/share/REAL-Video-Enhancer"
+    if os == 'Linux':
+        if flatpak == True:
+            if os.path.exists(f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer") == False:
+                os.system(f'mkdir -p "{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"')
+            return f"{homedir}/.var/app/io.github.tntwise.REAL-Video-Enhancer"
+        return f"{homedir}/.local/share/REAL-Video-Enhancer"
+    if os == 'Darwin':
+        return f"{homedir}/Library/REAL-Video-Enhancer"
