a3e274c31652ca8b1e2c061399d1ee89045d9a4d
TNTwise <you@example.com>
2024-04-11 12:46:08 -0500

    add more interpolation factors for rife (may or may not work idk)

--- modules/RifeCUDA.py
+++ modules/RifeCUDA.py
@@ -35,7 +35,11 @@ def modelOptions(self):
     self.ui.FPSPreview.setText("FPS:")
     self.ui.ensembleHelpButton.hide()
     self.ui.Rife_Times.addItem("2X")
+    self.ui.Rife_Times.addItem("3X")
     self.ui.Rife_Times.addItem("4X")
+    self.ui.Rife_Times.addItem("5X")
+    self.ui.Rife_Times.addItem("6X")
+    self.ui.Rife_Times.addItem("7X")
     self.ui.Rife_Times.addItem("8X")
     model_list = []
     for i in os.listdir(f"{thisdir}/models/rife-cuda/"):
