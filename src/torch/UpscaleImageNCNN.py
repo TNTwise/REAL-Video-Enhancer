6d67e0617829dbfae18203c1c81c89c3aa0acb1c
tntwise <tntwise3@gmail.com>
2024-05-29 00:07:32 +0000

    fix ncnn

--- src/torch/UpscaleImageNCNN.py
+++ src/torch/UpscaleImageNCNN.py
@@ -5,17 +5,18 @@ import ncnn
 import cv2
 class UpscaleNCNN:
     def __init__(self, model: str, num_threads, scale, gpuid=0, width=1920, height=1080):
-        '''self.model = UPSCALE(
+        self.model = UPSCALE(
             gpuid=gpuid, model_str=model, num_threads=num_threads, scale=scale
-        )'''
+        )
         self.width = width
         self.height = height
-        model = model + '.param'
+        self.scale = scale
+        '''model = model + '.param'
         self.net = ncnn.Net()
         self.net.opt.use_vulkan_compute = True
         self.net.load_param(model.replace('.bin','.param'))
         self.net.load_model(model.replace('.param','.bin'))
-        
+        '''
 
     def NCNNImageMatFromNP(self,npArray: np.array) -> ncnn.Mat:
         return ncnn.Mat.from_pixels(
@@ -62,11 +63,13 @@ class UpscaleNCNN:
 
 
     def UpscaleImage(self, image):
-        image = np.ascontiguousarray(
-            np.frombuffer(image, dtype=np.uint8).reshape(self.height, self.width, 3)
-        )
+        
 
-        return self.ProcessNCNN(image)
+        return np.ascontiguousarray(
+            np.frombuffer(
+                self.model.process_bytes(image,self.width,self.height,3),
+                dtype=np.uint8).reshape(self.height*self.scale, self.width*self.scale, 3)
+        )
 
 
 class UpscaleCuganNCNN:
@@ -83,6 +86,7 @@ class UpscaleCuganNCNN:
     ):
         self.width = width
         self.height = height
+        self.scale = scale
         self.model = Realcugan(
             gpuid=gpuid,
             models_path=models_path,
@@ -93,7 +97,8 @@ class UpscaleCuganNCNN:
         )
 
     def UpscaleImage(self, image):
-        image = np.ascontiguousarray(
-            np.frombuffer(image, dtype=np.uint8).reshape(self.height, self.width, 3)
+        return np.ascontiguousarray(
+            np.frombuffer(
+                self.model.process_bytes(image,self.width,self.height,3),
+                dtype=np.uint8).reshape(self.height*self.scale, self.width*self.scale, 3)
         )
-        return np.ascontiguousarray(self.model.process_cv2(image))
