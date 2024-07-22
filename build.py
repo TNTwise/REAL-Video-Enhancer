e2a8d25eb878f4cb5f7dd85dce00ef109b1fe105
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-07-21 21:45:51 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,22 +1,17 @@
-
-
 import os
 import subprocess
 import argparse
-
 
 def pipInstall(file):
     command = ["python3", "-m", "pip", "install", "-r"]
     command.append(file)
     subprocess.run(command)
 
-
 def installImportLib():
     command = ["python3", "-m", "pip", "install", "--upgrade", "pip", "setuptools"]
     subprocess.run(command)
     command = ["python3", "-m", "pip", "install", "importlib"]
     subprocess.run(command)
-
 
 def buildenv():
     filelist = [
@@ -30,29 +25,21 @@ def buildenv():
         with open(f"{file}.py", "w") as f:
             uic.compileUi(f"{file}.ui", f)
 
-
 def buildNCNNLinux():
     """
     Builds REAL Video Enhancer with only ncnn, needs python 3.8
     """
     pipInstall("requirements-NCNN-Linux.txt")
-    command = ["python3", "-m", "cx_Freeze", "-c", "main.py", "--target-dir", "dist"]
+    command = ["python3","-m", "cx_Freeze","-c","main.py","--target-dir","dist"]
     subprocess.run(command)
-
-
-def fixEvalFrame():
-    with open(
-        os.path.join("dist", "main", "_internal", "torch", "_dynamo", "eval_frame.py"),
-        "w",
-    ) as f:
-        f.write(fixedEvalFrame)
-
+    
 
 def buildCUDALinux():
     """
-    Builds REAL Video Enhancer with cuda, needs python 3.11
+    Builds REAL Video Enhancer with cuda, needs python 3.10
     """
     pipInstall("requirements-CUDA-Linux.txt")
+    installImportLib()
     command = [
         "python3",
         "-m",
@@ -64,8 +51,6 @@ def buildCUDALinux():
         "polygraphy",
         "--collect-all",
         "torch",
-        "--collect-all",
-        "torch._C",
         "--collect-all",
         "tensorrt",
         "--collect-all",
@@ -120,56 +105,49 @@ def buildNCNNFlatpakLinux():
 
 def buildNCNNMacOS():
     command = [
-        "python3",
-        "-m",
-        "PyInstaller",
-        "--onefile",
-        "main.py",
-        "--hidden-import",
-        "upscale_ncnn_py.upscale_ncnn_py_wrapper",
-        "--hidden-import",
-        "realcugan_ncnn_py.realcugan_ncnn_py_wrapper",
-        "--hidden-import",
-        "rife_ncnn_vulkan_python.rife_ncnn_vulkan_wrapper",
+    'python3', '-m', 'PyInstaller',
+    '--onefile', 'main.py',
+    '--hidden-import', 'upscale_ncnn_py.upscale_ncnn_py_wrapper',
+    '--hidden-import', 'realcugan_ncnn_py.realcugan_ncnn_py_wrapper',
+    '--hidden-import', 'rife_ncnn_vulkan_python.rife_ncnn_vulkan_wrapper'
     ]
     subprocess.run(command)
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Build RVE")
-    parser.add_argument(
-        "--build_cuda",
-        help="builds cuda rve, requires python 3.10",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--build_ncnn",
-        help="builds ncnn rve, requires python 3.8",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--build_rocm",
-        help="builds rocm rve, requires python 3.11",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--build_mac_ncnn",
-        help="builds macos ncnn rve, requires python 3.11",
-        action="store_true",
-    )
-
-    args = parser.parse_args()
-
     pipInstall("requirements.txt")
     import PyQt5.uic as uic
-
     buildenv()
+    parser = argparse.ArgumentParser(
+            description="Build RVE"
+        )
+    parser.add_argument(
+            "--build_cuda",
+            help="builds cuda rve, requires python 3.10",
+            action="store_true",
+        )
+    parser.add_argument(
+            "--build_ncnn",
+            help="builds ncnn rve, requires python 3.8",
+            action="store_true",
+        )
+    parser.add_argument(
+            "--build_rocm",
+            help="builds rocm rve, requires python 3.10",
+            action="store_true",
+        )
+    parser.add_argument(
+            "--build_mac_ncnn",
+            help="builds macos ncnn rve, requires python 3.11",
+            action="store_true",
+        )
+    
+    args = parser.parse_args()
+    
     if args.build_mac_ncnn:
         buildNCNNMacOS()
     if args.build_rocm:
         buildROCmLinux()
     if args.build_cuda:
         buildCUDALinux()
-        #fixEvalFrame()
     if args.build_ncnn:
         buildNCNNLinux()
