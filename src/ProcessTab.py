19385f37de4a280c7150d81aee851303a16871e6
tntwise <tntwise3@gmail.com>
2024-07-28 13:25:21 -0500

    Revert "fix stuff (backend fails idk why)"
    This reverts commit 1db711c2a1c0813c685a0968278fae279edfb650.

--- src/ProcessTab.py
+++ src/ProcessTab.py
@@ -63,7 +63,7 @@ class ProcessTab:
             "-i",
             self.inputFile,
             "-o",
-            "our.mp4",
+            "PIPE",
             "--interpolateModel",
             os.path.join(modelsPath(),"rife-v4.20-ncnn"),  # put actual model here, this is a placeholder
             "-b",
