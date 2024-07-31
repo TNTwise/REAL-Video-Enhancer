52c1c02958a5d23945d652b0c15107b62b912fd1
tntwise <tntwise3@gmail.com>
2024-07-31 02:58:44 +0000

    make preview more efficient

--- src/ProcessTab.py
+++ src/ProcessTab.py
@@ -7,33 +7,62 @@ import numpy as np
 import cv2
 from multiprocessing import shared_memory
 import time
-from PySide6.QtCore import QThread, Signal, QMutex, QMutexLocker
-from PySide6.QtCore import QThread, Signal, Qt
+from PySide6.QtCore import QThread, Signal, QMutex, QMutexLocker, Qt
 from PySide6 import QtGui
 
 from .Util import ffmpegPath, pythonPath, currentDirectory, modelsPath
 
 
 class UpdateGUIThread(QThread):
-    updateGUITick = Signal()
+    """
+    Gets the latest bytes outputed from the shared memory and returns them in QImage format for display
+    """
+    latestPreviewPixmap = Signal(QtGui.QImage)
 
-    def __init__(self):
+    def __init__(self, imagePreviewSharedMemoryID, outputVideoHeight,outputVideoWidth):
         super().__init__()
         self._stop_flag = False  # Boolean flag to control stopping
         self._mutex = QMutex()  # Atomic flag to control stopping
-
+        self.imagePreviewSharedMemoryID = imagePreviewSharedMemoryID
+        self.outputVideoHeight = outputVideoHeight
+        self.outputVideoWidth = outputVideoWidth
     def run(self):
         while True:
             with QMutexLocker(self._mutex):
                 if self._stop_flag:
                     break
+            try:
+                self.shm = shared_memory.SharedMemory(name=self.imagePreviewSharedMemoryID)
+                image_bytes = self.shm.buf[:].tobytes()
+
+                # Convert image bytes back to numpy array
+                image_array = np.frombuffer(image_bytes, dtype=np.uint8).reshape(
+                    (self.outputVideoHeight, self.outputVideoWidth, 3)
+                )
+                pixmap = self.convert_cv_qt(image_array)
+                self.latestPreviewPixmap.emit(pixmap)
+            except FileNotFoundError:
+                #print("preview not available")
+                pass
             time.sleep(0.1)
-            self.updateGUITick.emit()
+            
+            
+            
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv_img
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(
+            rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888
+        )
+        return convert_to_Qt_format
 
     def stop(self):
         with QMutexLocker(self._mutex):
             self._stop_flag = True
-
+        self.shm.close()
+        print("Closed Read Memory")
 
 class ProcessTab:
     def __init__(
@@ -71,7 +100,8 @@ class ProcessTab:
         self.videoFrameCount = videoFrameCount
         self.upscaleTimes = upscaleTimes
         self.interpolateTimes = interpolateTimes
-        self.frameChunkSize = videoHeight * videoWidth * 3
+        self.outputVideoWidth = videoWidth * upscaleTimes
+        self.outputVideoHeight = videoHeight * upscaleTimes
         """
         Function to start the rendering process
         It will initially check for any issues with the current setup, (invalid file, no permissions, etc..)
@@ -88,8 +118,12 @@ class ProcessTab:
         self.startGUIUpdate()
 
     def startGUIUpdate(self):
-        self.workerThread = UpdateGUIThread()
-        self.workerThread.updateGUITick.connect(self.updateProcessTab)
+        
+        self.workerThread = UpdateGUIThread(imagePreviewSharedMemoryID=self.imagePreviewSharedMemoryID,
+                                            outputVideoHeight=self.outputVideoHeight,
+                                            outputVideoWidth=self.outputVideoWidth
+                                            )
+        self.workerThread.latestPreviewPixmap.connect(self.updateProcessTab)
         self.workerThread.finished.connect(self.workerThread.deleteLater)
         self.workerThread.finished.connect(self.workerThread.quit)
         self.workerThread.finished.connect(
@@ -101,8 +135,7 @@ class ProcessTab:
         self.workerThread.stop()
         self.workerThread.quit()
         self.workerThread.wait()
-        self.shm.close()
-        print("Closed Read Memory")
+        
 
     def renderToPipeThread(self):
         command = [
@@ -130,31 +163,15 @@ class ProcessTab:
         print("Done with render")
         self.onRenderCompletion()
 
-    def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv_img
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(
-            rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888
-        )
-        p = convert_to_Qt_format.scaled(600, 600, Qt.KeepAspectRatio)
-        return QtGui.QPixmap.fromImage(p)
 
-    def updateProcessTab(self):
+    def updateProcessTab(self, qimage: QtGui.QImage):
         """
         Called by the worker QThread, and updates the GUI elements: Progressbar, Preview, FPS
         """
         try:
-            self.shm = shared_memory.SharedMemory(name=self.imagePreviewSharedMemoryID)
-            image_bytes = self.shm.buf[:].tobytes()
-
-            # Convert image bytes back to numpy array
-            image_array = np.frombuffer(image_bytes, dtype=np.uint8).reshape(
-                (self.videoHeight, self.videoWidth, 3)
-            )
-
-            self.parent.previewLabel.setPixmap(self.convert_cv_qt(image_array))
+            p = qimage.scaled(600, 600, Qt.KeepAspectRatio)
+            
+            self.parent.previewLabel.setPixmap(QtGui.QPixmap.fromImage(p))
         except FileNotFoundError:
             # print("No preview yet!")
             pass
