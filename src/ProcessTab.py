370a8a18de9c88424f96aaf93e8d1c7423981689
tntwise <tntwise3@gmail.com>
2024-08-08 08:35:47 -0500

    4.22

--- src/ProcessTab.py
+++ src/ProcessTab.py
@@ -35,6 +35,7 @@ class ProcessTab:
             "RIFE 4.18": ("rife-v4.18", "rife-v4.18.tar.gz", 1, "rife413"),
             "RIFE 4.20": ("rife-v4.20", "rife-v4.20.tar.gz", 1, "rife420"),
             "RIFE 4.21": ("rife-v4.21", "rife-v4.21.tar.gz", 1, "rife421"),
+            "RIFE 4.22": ("rife-v4.22", "rife-v4.22.tar.gz", 1, "rife421"),
         }
         self.pytorchInterpolateModels = {
             "RIFE 4.6": ("rife4.6.pkl", "rife4.6.pkl", 1, "rife46"),
@@ -43,6 +44,7 @@ class ProcessTab:
             "RIFE 4.18": ("rife4.18.pkl", "rife4.18.pkl", 1, "rife413"),
             "RIFE 4.20": ("rife4.20.pkl", "rife4.20.pkl", 1, "rife420"),
             "RIFE 4.21": ("rife4.21.pkl", "rife4.21.pkl", 1, "rife421"),
+            "RIFE 4.22": ("rife4.22.pkl", "rife4.22.pkl", 1, "rife421"),
         }
         self.ncnnUpscaleModels = {
             "SPAN (Animation) (2X)": (
