03bbda3aa8a6ff131da6a38a680cc84d9faf156d
tntwise <tntwise3@gmail.com>
2024-08-23 02:10:09 +0000

    more streams

--- backend/src/InterpolateTorch.py
+++ backend/src/InterpolateTorch.py
@@ -119,163 +119,163 @@ class InterpolateRifeTorch:
         scale = 1
         if UHDMode:
             scale = 0.5
-
-        state_dict = torch.load(
-            interpolateModelPath, map_location=self.device, weights_only=True, mmap=True
-        )
-
-        tmp = max(32, int(32 / scale))
-        self.pw = math.ceil(self.width / tmp) * tmp
-        self.ph = math.ceil(self.height / tmp) * tmp
-        self.padding = (0, self.pw - self.width, 0, self.ph - self.height)
-
-        # detect what rife arch to use
-        match interpolateArch:
-            case "rife46":
-                from .InterpolateArchs.RIFE.rife46IFNET import IFNet
-
-                v1 = True
-            case "rife47":
-                from .InterpolateArchs.RIFE.rife47IFNET import IFNet
-
-                v1 = True
-            case "rife413":
-                from .InterpolateArchs.RIFE.rife413IFNET import IFNet
-
-                v1 = False
-            case "rife420":
-                from .InterpolateArchs.RIFE.rife420IFNET import IFNet
-
-                v1 = False
-            case "rife421":
-                from .InterpolateArchs.RIFE.rife421IFNET import IFNet
-
-                v1 = False
-            case "rife422-lite":
-                from .InterpolateArchs.RIFE.rife422_liteIFNET import IFNet
-
-                v1 = False
-            case _:
-                errorAndLog("Invalid Interpolation Arch")
-
-        # if 4.6 v1
-        if v1:
-            self.tenFlow_div = torch.tensor(
-                [(self.pw - 1.0) / 2.0, (self.ph - 1.0) / 2.0],
-                dtype=self.dtype,
-                device=self.device,
-            )
-            tenHorizontal = (
-                torch.linspace(-1.0, 1.0, self.pw, dtype=self.dtype, device=self.device)
-                .view(1, 1, 1, self.pw)
-                .expand(-1, -1, self.ph, -1)
-            ).to(dtype=self.dtype, device=self.device)
-            tenVertical = (
-                torch.linspace(-1.0, 1.0, self.ph, dtype=self.dtype, device=self.device)
-                .view(1, 1, self.ph, 1)
-                .expand(-1, -1, -1, self.pw)
-            ).to(dtype=self.dtype, device=self.device)
-            self.backwarp_tenGrid = torch.cat([tenHorizontal, tenVertical], 1)
-
-        else:
-            # if v2
-            h_mul = 2 / (self.pw - 1)
-            v_mul = 2 / (self.ph - 1)
-            self.tenFlow_div = (
-                torch.Tensor([h_mul, v_mul])
-                .to(device=self.device, dtype=self.dtype)
-                .reshape(1, 2, 1, 1)
+        with torch.cuda.stream(self.prepareStream):
+            state_dict = torch.load(
+                interpolateModelPath, map_location=self.device, weights_only=True, mmap=True
             )
 
-            self.backwarp_tenGrid = torch.cat(
-                (
-                    (torch.arange(self.pw) * h_mul - 1)
-                    .reshape(1, 1, 1, -1)
-                    .expand(-1, -1, self.ph, -1),
-                    (torch.arange(self.ph) * v_mul - 1)
-                    .reshape(1, 1, -1, 1)
-                    .expand(-1, -1, -1, self.pw),
-                ),
-                dim=1,
-            ).to(device=self.device, dtype=self.dtype)
+            tmp = max(32, int(32 / scale))
+            self.pw = math.ceil(self.width / tmp) * tmp
+            self.ph = math.ceil(self.height / tmp) * tmp
+            self.padding = (0, self.pw - self.width, 0, self.ph - self.height)
 
-        self.flownet = IFNet(
-            scale=scale,
-            ensemble=ensemble,
-            dtype=self.dtype,
-            device=self.device,
-            width=self.width,
-            height=self.height,
-            backwarp_tenGrid=self.backwarp_tenGrid,
-            tenFlow_div=self.tenFlow_div,
-        )
+            # detect what rife arch to use
+            match interpolateArch:
+                case "rife46":
+                    from .InterpolateArchs.RIFE.rife46IFNET import IFNet
 
-        state_dict = {
-            k.replace("module.", ""): v for k, v in state_dict.items() if "module." in k
-        }
-        self.flownet.load_state_dict(state_dict=state_dict, strict=False)
-        self.flownet.eval().to(device=self.device, dtype=self.dtype)
+                    v1 = True
+                case "rife47":
+                    from .InterpolateArchs.RIFE.rife47IFNET import IFNet
 
-        if self.backend == "tensorrt":
-            import tensorrt
-            import torch_tensorrt
+                    v1 = True
+                case "rife413":
+                    from .InterpolateArchs.RIFE.rife413IFNET import IFNet
 
-            trt_engine_path = os.path.join(
-                os.path.realpath(trt_cache_dir),
-                (
-                    f"{os.path.basename(self.interpolateModel)}"
-                    + f"_{self.pw}x{self.ph}"
-                    + f"_{'fp16' if self.dtype == torch.float16 else 'fp32'}"
-                    + f"_scale-{scale}"
-                    + f"_ensemble-{ensemble}"
-                    + f"_{torch.cuda.get_device_name(self.device)}"
-                    + f"_trt-{tensorrt.__version__}"
-                    + (
-                        f"_workspace-{trt_workspace_size}"
-                        if trt_workspace_size > 0
-                        else ""
-                    )
-                    + (
-                        f"_aux-{trt_max_aux_streams}"
-                        if trt_max_aux_streams is not None
-                        else ""
-                    )
-                    + (
-                        f"_level-{trt_optimization_level}"
-                        if trt_optimization_level is not None
-                        else ""
-                    )
-                    + ".ts"
-                ),
-            )
-            if not os.path.isfile(trt_engine_path):
-                inputs = [
-                    torch.zeros(
-                        (1, 3, self.ph, self.pw), dtype=self.dtype, device=device
-                    ),
-                    torch.zeros(
-                        (1, 3, self.ph, self.pw), dtype=self.dtype, device=device
-                    ),
-                    torch.zeros(
-                        (1, 1, self.ph, self.pw), dtype=self.dtype, device=device
-                    ),
-                ]
-                self.flownet = torch_tensorrt.compile(
-                    self.flownet,
-                    ir="dynamo",
-                    inputs=inputs,
-                    enabled_precisions={self.dtype},
-                    debug=trt_debug,
-                    workspace_size=trt_workspace_size,
-                    min_block_size=1,
-                    max_aux_streams=trt_max_aux_streams,
-                    optimization_level=trt_optimization_level,
-                    device=device,
+                    v1 = False
+                case "rife420":
+                    from .InterpolateArchs.RIFE.rife420IFNET import IFNet
+
+                    v1 = False
+                case "rife421":
+                    from .InterpolateArchs.RIFE.rife421IFNET import IFNet
+
+                    v1 = False
+                case "rife422-lite":
+                    from .InterpolateArchs.RIFE.rife422_liteIFNET import IFNet
+
+                    v1 = False
+                case _:
+                    errorAndLog("Invalid Interpolation Arch")
+
+            # if 4.6 v1
+            if v1:
+                self.tenFlow_div = torch.tensor(
+                    [(self.pw - 1.0) / 2.0, (self.ph - 1.0) / 2.0],
+                    dtype=self.dtype,
+                    device=self.device,
+                )
+                tenHorizontal = (
+                    torch.linspace(-1.0, 1.0, self.pw, dtype=self.dtype, device=self.device)
+                    .view(1, 1, 1, self.pw)
+                    .expand(-1, -1, self.ph, -1)
+                ).to(dtype=self.dtype, device=self.device)
+                tenVertical = (
+                    torch.linspace(-1.0, 1.0, self.ph, dtype=self.dtype, device=self.device)
+                    .view(1, 1, self.ph, 1)
+                    .expand(-1, -1, -1, self.pw)
+                ).to(dtype=self.dtype, device=self.device)
+                self.backwarp_tenGrid = torch.cat([tenHorizontal, tenVertical], 1)
+
+            else:
+                # if v2
+                h_mul = 2 / (self.pw - 1)
+                v_mul = 2 / (self.ph - 1)
+                self.tenFlow_div = (
+                    torch.Tensor([h_mul, v_mul])
+                    .to(device=self.device, dtype=self.dtype)
+                    .reshape(1, 2, 1, 1)
                 )
 
-                torch_tensorrt.save(self.flownet, trt_engine_path, inputs=inputs)
+                self.backwarp_tenGrid = torch.cat(
+                    (
+                        (torch.arange(self.pw) * h_mul - 1)
+                        .reshape(1, 1, 1, -1)
+                        .expand(-1, -1, self.ph, -1),
+                        (torch.arange(self.ph) * v_mul - 1)
+                        .reshape(1, 1, -1, 1)
+                        .expand(-1, -1, -1, self.pw),
+                    ),
+                    dim=1,
+                ).to(device=self.device, dtype=self.dtype)
 
-            self.flownet = torch.export.load(trt_engine_path).module()
+            self.flownet = IFNet(
+                scale=scale,
+                ensemble=ensemble,
+                dtype=self.dtype,
+                device=self.device,
+                width=self.width,
+                height=self.height,
+                backwarp_tenGrid=self.backwarp_tenGrid,
+                tenFlow_div=self.tenFlow_div,
+            )
+
+            state_dict = {
+                k.replace("module.", ""): v for k, v in state_dict.items() if "module." in k
+            }
+            self.flownet.load_state_dict(state_dict=state_dict, strict=False)
+            self.flownet.eval().to(device=self.device, dtype=self.dtype)
+
+            if self.backend == "tensorrt":
+                import tensorrt
+                import torch_tensorrt
+
+                trt_engine_path = os.path.join(
+                    os.path.realpath(trt_cache_dir),
+                    (
+                        f"{os.path.basename(self.interpolateModel)}"
+                        + f"_{self.pw}x{self.ph}"
+                        + f"_{'fp16' if self.dtype == torch.float16 else 'fp32'}"
+                        + f"_scale-{scale}"
+                        + f"_ensemble-{ensemble}"
+                        + f"_{torch.cuda.get_device_name(self.device)}"
+                        + f"_trt-{tensorrt.__version__}"
+                        + (
+                            f"_workspace-{trt_workspace_size}"
+                            if trt_workspace_size > 0
+                            else ""
+                        )
+                        + (
+                            f"_aux-{trt_max_aux_streams}"
+                            if trt_max_aux_streams is not None
+                            else ""
+                        )
+                        + (
+                            f"_level-{trt_optimization_level}"
+                            if trt_optimization_level is not None
+                            else ""
+                        )
+                        + ".ts"
+                    ),
+                )
+                if not os.path.isfile(trt_engine_path):
+                    inputs = [
+                        torch.zeros(
+                            (1, 3, self.ph, self.pw), dtype=self.dtype, device=device
+                        ),
+                        torch.zeros(
+                            (1, 3, self.ph, self.pw), dtype=self.dtype, device=device
+                        ),
+                        torch.zeros(
+                            (1, 1, self.ph, self.pw), dtype=self.dtype, device=device
+                        ),
+                    ]
+                    self.flownet = torch_tensorrt.compile(
+                        self.flownet,
+                        ir="dynamo",
+                        inputs=inputs,
+                        enabled_precisions={self.dtype},
+                        debug=trt_debug,
+                        workspace_size=trt_workspace_size,
+                        min_block_size=1,
+                        max_aux_streams=trt_max_aux_streams,
+                        optimization_level=trt_optimization_level,
+                        device=device,
+                    )
+
+                    torch_tensorrt.save(self.flownet, trt_engine_path, inputs=inputs)
+
+                self.flownet = torch.export.load(trt_engine_path).module()
 
     def handlePrecision(self, precision):
         if precision == "auto":
