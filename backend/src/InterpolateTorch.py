ee2a9c9aa107e560f208aa09593ae9bf97b56413
TNTwise <tntwise3@gmail.com>
2024-09-15 19:18:53 -0500

    regular trt level

--- backend/src/InterpolateTorch.py
+++ backend/src/InterpolateTorch.py
@@ -356,7 +356,7 @@ class InterpolateRifeTorch:
                             else ""
                         )
                         + (
-                            f"_level-5"
+                            f"_level-{self.trt_optimization_level}"
                             if self.trt_optimization_level is not None
                             else ""
                         )
@@ -377,7 +377,7 @@ class InterpolateRifeTorch:
                             workspace_size=self.trt_workspace_size,
                             min_block_size=1,
                             max_aux_streams=self.trt_max_aux_streams,
-                            optimization_level=5,
+                            optimization_level=self.trt_optimization_level,
                             device=self.device,
                             cache_built_engines=False,
                             reuse_cached_engines=False,
