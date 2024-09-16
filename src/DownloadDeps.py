de2f244c3d5b467d75c758edaea6621cf08aa303
TNTwise <tntwise3@gmail.com>
2024-09-15 22:25:29 -0500

    add spandrel deps

--- src/DownloadDeps.py
+++ src/DownloadDeps.py
@@ -176,6 +176,8 @@ class DownloadDependencies:
             "https://download.pytorch.org/whl/nightly/cu124_pypi_pkg/torch-2.5.0.dev20240826%2Bcu124-cp311-cp311-linux_x86_64.whl",
             "https://download.pytorch.org/whl/nightly/cu124/torchvision-0.20.0.dev20240826%2Bcu124-cp311-cp311-linux_x86_64.whl",
             "https://download.pytorch.org/whl/nightly/cu124_pypi_pkg/torch_no_python-2.5.0.dev20240826%2Bcu124-py3-none-any.whl",
+            "safetensors",
+            "einops",
         ]
         torchCUDAWindowsDeps = [
             #"https://github.com/TNTwise/real-video-enhancer-models/releases/download/models/spandrel-0.3.4-py3-none-any.whl",
@@ -185,8 +187,8 @@ class DownloadDependencies:
             "https://download.pytorch.org/whl/nightly/cu124/torchvision-0.20.0.dev20240826%2Bcu124-cp311-cp311-win_amd64.whl",
             # "torch==2.4.0",
             # "torchvision==0.19.0",
-            # "safetensors",
-            # "einops",
+            "safetensors",
+            "einops",
         ]
         match getPlatform():
             case "win32":
