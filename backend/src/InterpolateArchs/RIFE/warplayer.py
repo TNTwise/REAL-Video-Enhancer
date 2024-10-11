5dc5a678f8d312d31628c80319a7021df49cd5f8
TNTwise <tntwise3@gmail.com>
2024-10-11 10:40:22 -0500

    force fp32 on warp

--- backend/src/InterpolateArchs/RIFE/warplayer.py
+++ backend/src/InterpolateArchs/RIFE/warplayer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 
 
-'''def warp(tenInput, tenFlow, tenFlow_div, backwarp_tenGrid):
+def warp(tenInput, tenFlow, tenFlow_div, backwarp_tenGrid):
     dtype = tenInput.dtype
     tenInput = tenInput.to(torch.float)
     tenFlow = tenFlow.to(torch.float)
@@ -10,9 +10,9 @@ import torch.nn.functional as F
     tenFlow = torch.cat([tenFlow[:, 0:1] / tenFlow_div[0], tenFlow[:, 1:2] / tenFlow_div[1]], 1)
     g = (backwarp_tenGrid + tenFlow).permute(0, 2, 3, 1)
     grid_sample = F.grid_sample(input=tenInput, grid=g, mode="bilinear", padding_mode="border", align_corners=True)
-    return grid_sample.to(dtype)'''
+    return grid_sample.to(dtype)
 
-def warp(tenInput, tenFlow, tenFlow_div, backwarp_tenGrid):
+"""def warp(tenInput, tenFlow, tenFlow_div, backwarp_tenGrid):
     tenFlow = torch.cat(
         [tenFlow[:, 0:1] / tenFlow_div[0], tenFlow[:, 1:2] / tenFlow_div[1]], 1
     )
@@ -25,3 +25,4 @@ def warp(tenInput, tenFlow, tenFlow_div, backwarp_tenGrid):
         padding_mode="border",
         align_corners=True,
     )
+"""
