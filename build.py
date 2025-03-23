0444a8d6110a51d9a2ce1bc68409121a236255ae
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-03-22 21:34:34 -0700

    fix windows with noconsole

--- build.py
+++ build.py
@@ -194,7 +194,7 @@ class PyInstaller(BuildManager):
             + " --icon=icons/logo-v2.ico" 
             + " --noconfirm"
             + " --noupx" 
-            + " --noconsole"
+            # + " --noconsole"
             + " --distpath"
             + f" {OUTPUT_FOLDER}"
             )
