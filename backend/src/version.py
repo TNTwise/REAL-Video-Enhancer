2d3ffc88d900d86010a875a98d41f8ea4a1c237e
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-10-24 23:49:07 -0700

    Bump backend version to 2.3.9-dev13

--- backend/src/version.py
+++ backend/src/version.py
@@ -1,2 +1,2 @@
-__version__ = "2.3.8-dev13" # this is the version of the backend, this is compared to the version of the front end. need to be the same
+__version__ = "2.3.9-dev13" # this is the version of the backend, this is compared to the version of the front end. need to be the same
 
