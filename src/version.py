187acbbd02d39989996beb262b38ef833b2f8102
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-10-24 23:48:56 -0700

    Bump version to 2.3.9
    Updated version and backend_dev_version to 2.3.9.

--- src/version.py
+++ src/version.py
@@ -1,2 +1,2 @@
-version = "2.3.8"
-backend_dev_version = "2.3.8-dev13"  # has to match version of backend, update this wehenver updating pre release
+version = "2.3.9"
+backend_dev_version = "2.3.9-dev13"  # has to match version of backend, update this wehenver updating pre release
