255598ed2c961a08e7657ca9765360f85d31e749
TNTwise <tntwise3@gmail.com>
2025-11-19 22:50:02 -0600

    undo temp dir changes

--- src/constants.py
+++ src/constants.py
@@ -20,9 +20,9 @@ LOCKFILE = QDir.tempPath() + "/REAL-Video-Enhancer.lock"
 
 
 PLATFORM = sys.platform  # win32, darwin, linux
-
-IS_FLATPAK = "FLATPAK_ID" in os.environ
 IS_STEAM = "SteamAppId" in os.environ
+IS_FLATPAK = "FLATPAK_ID" in os.environ and not IS_STEAM
+
 HOME_PATH = os.path.expanduser("~")
 
 IS_COMPILED_OR_FROZEN = hasattr(sys, "frozen")
