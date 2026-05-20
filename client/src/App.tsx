5b6ebed88bbc272ea6fd2c52c740766c63820d3d
TNTwise <tntwise3@gmail.com>
2026-05-19 21:00:28 -0500

    add file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import rveLogo from "./assets/logo-v2.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./styles/App.css";
+import PageSelectorNavBar from "./components/PageSelectorNavBar/PageSelectorNavBar";
 
 function App() {
   return (
     <div className="App">
+      <PageSelectorNavBar />
       <h1>
         REAL Video Enhancer
-        
       </h1>
       <img src={rveLogo} className="logo" alt="REAL Video Enhancer logo" />
     </div>
---
+++ client/src/components/PageSelectorNavBar/PageSelectorNavBar.tsx
@@ -0,0 +1,10 @@
+import "./PageSelectorNavBar.css";
+
+function PageSelectorNavBar() {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav"></ul>
+        </nav>
+    );
+}
+export default PageSelectorNavBar;
