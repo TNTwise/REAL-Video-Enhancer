bdd78da9cd5a69fdb598425fbc5ad830b54507c7
TNTWise <tntwise3@gmail.com>
2026-06-03 23:09:10 -0500

    add layout

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/PageSelectorNavBar/PageSelectorNavBar";
-import homeIcon from '/src/assets/icons/home.svg';
-import settingsIcon from '/src/assets/icons/settings.svg';
-import downloadIcon from '/src/assets/icons/download.svg';
+
 
 
 export default function RootLayout() {
