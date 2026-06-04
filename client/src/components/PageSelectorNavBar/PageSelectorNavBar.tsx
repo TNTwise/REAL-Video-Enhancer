872f8c424562ebc3753c0ea3a8bb59df0300a5bd
TNTWise <tntwise3@gmail.com>
2026-06-04 00:04:23 -0500

    style navbar

--- client/src/components/PageSelectorNavBar/PageSelectorNavBar.tsx
+++ client/src/components/PageSelectorNavBar/PageSelectorNavBar.tsx
@@ -1,28 +1,48 @@
-import { HStack, Link as ChakraLink } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom"; // 1. Import React Router's Link
-import { primaryBGColor } from "@/styles/styles";
+import { Image, HStack, Link as ChakraLink } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  primaryBGColor,
+  primaryWidgetColor,
+  secondaryWidgetColor,
+} from "@/styles/styles";
+import homeIcon from "@/assets/icons/home.svg";
+import downloadIcon from "@/assets/icons/download.svg";
+
 const links = [
-  { name: "Home", href: "/home" },
-  { name: "Download", href: "/download" },
+  { name: "Home", href: "/home", image: homeIcon },
+  { name: "Download", href: "/download", image: downloadIcon },
 ];
 
 export const Navbar = () => {
   return (
-    <HStack gap={8} align="center" bg={primaryBGColor}>
+    <HStack
+      gap={8}
+      align="center"
+      bg={primaryBGColor}
+      padding="10px"
+      borderRadius="10px"
+      marginBottom="10px"
+    >
       {links.map((link) => (
         <ChakraLink
           key={link.name}
           as={RouterLink}
           to={link.href} // ignore
           fontWeight="medium"
-          color="blue.600"
-          _hover={{
-            color: "blue.500",
-            textDecoration: "underline",
-          }}
           transition="color 0.2s ease"
         >
-          {link.name}
+          <Image
+            background={primaryWidgetColor}
+            padding="5px"
+            borderRadius="5px"
+            _hover={{
+              background: secondaryWidgetColor,
+            }}
+            transition="color 2s ease"
+            src={link.image}
+            w="50px"
+            h="50px"
+          ></Image>
         </ChakraLink>
       ))}
     </HStack>
--- client/src/styles/styles.ts
+++ client/src/styles/styles.ts
@@ -1,5 +1,5 @@
 export const primaryBGColor = "rgb(21, 24, 29)";
 export const secondaryBGColor = "rgb(31, 35, 44)";
-export const primaryWidget = "rgb(31, 35, 44)";
-export const secondaryWidget = "rgb(43, 49, 61)";
+export const primaryWidgetColor = "rgb(31, 35, 44)";
+export const secondaryWidgetColor = "rgb(43, 49, 61)";
 export const textColor = "rgb(255, 255, 255)";
