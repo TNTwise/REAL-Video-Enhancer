b6ffffe5792c9ceddab0ab9237f3110255b4bf49
TNTWise <tntwise3@gmail.com>
2026-06-03 21:36:58 -0500

    add pages

---
+++ client/src/pages/DownloadPage/DownloadPage.tsx
@@ -0,0 +1,7 @@
+export function DownloadPage () {
+    return (
+        <div>
+            <h1>download page</h1>
+        </div>
+    );
+}
---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+export function HomePage () {
+    return (
+        <div>
+            <h1>home ahh page</h1>
+        </div>
+    );
+}
