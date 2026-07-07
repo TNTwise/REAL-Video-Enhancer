07177e8970e6faa0d2de08736663f1ab5986097b
TNTWise <tntwise3@gmail.com>
2026-07-06 22:43:53 -0500

    stuff

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
-import { Navbar } from "../components/PageSelectorNavBar/PageSelectorNavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Home, Cpu, Settings, Download } from "lucide-react";
+import Dock from "../components/Dock/Dock";
 
-
+const dockItems = [
+  { icon: <Home size={18} />, label: 'Home', onClick: () => {} },
+  { icon: <Cpu size={18} />, label: 'System', onClick: () => {} },
+  { icon: <Settings size={18} />, label: 'Settings', onClick: () => {} },
+  { icon: <Download size={18} />, label: 'Downloads', onClick: () => {} },
+];
 
 export default function RootLayout() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet />
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const items = dockItems.map(item => ({
+    ...item,
+    onClick: item.label === 'Home' ? () => navigate('/home')
+      : item.label === 'Downloads' ? () => navigate('/download')
+      : item.onClick
+  }));
+
+  return (
+    <div>
+      <Dock items={items} />
+      <Outlet />
+    </div>
+  );
 }
